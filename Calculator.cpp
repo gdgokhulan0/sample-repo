03201ddc87a536c6b886e9947bd78951482ac689
dev.gdg0 <50659090+gdgokhulan0@users.noreply.github.com>
2021-06-02 09:38:50 +0530

    Initial Commit

---
+++ Calculator.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Calculator
+{
+public:
+    int a, b;
+    Calculator(int a, int b)
+    {
+        this.a = a;
+        this.b = b;
+    }
+};
+
+int main()
+{
+    Calculator calc = new Calculator(4, 2);
+}
